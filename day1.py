e85e2c76c5404c123dc309cadf3bd43f366c59bc
Raymond Lam <raymond.lam@arista.com>
2022-12-13 16:21:49 +1100

    Solution for day 1

---
+++ day1.py
@@ -0,0 +1,23 @@
+txtInput = ''
+with open('day1input.txt', 'r+') as f:
+    txtInput = f.read()
+
+# split by the empty lines
+elves = txtInput.split('\n\n')
+
+maxCalories = {
+    "calory": float('-inf'),
+    "elfMax": 0
+}
+
+for idx, elf in enumerate(elves):
+    calory = 0
+    for calories in elf.split('\n'):
+        calory += int(calories)
+    if calory > maxCalories["calory"]:
+        maxCalories['calory'] = calory
+        maxCalories['elfMax'] = idx
+
+print(
+    f'Elf {maxCalories["elfMax"]} has the most calories with {maxCalories["calory"]} calories'
+)
