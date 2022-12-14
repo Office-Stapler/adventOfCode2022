aa37ea2e3ad51f0d748544f3af90a2d403e2f4dc
Raymond Lam <raymond.lam@outlook.com.au>
2022-12-14 12:49:47 +1100

    Day 6 solution

---
+++ day6p1.py
@@ -0,0 +1,17 @@
+txt_input = ''
+with open('day6input.txt', 'r+') as f:
+    txt_input = f.read()
+data_stream = txt_input
+checked_index = 0
+
+def is_marker(chars: str) -> bool:
+    return len(set(chars)) == len(chars)
+
+while checked_index < len(data_stream):
+    processed_chars = data_stream[checked_index:checked_index + 4]
+    if is_marker(processed_chars):
+        print(data_stream[checked_index:checked_index + 4])
+        print(f'Processed characters: {checked_index + 4}')
+        break
+    checked_index += 1
+    
